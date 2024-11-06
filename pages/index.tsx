7f30dbe583b65444f25404bcd2d59be90f9523c9
IbadUddinSiddqui <160363222+IbadUddinSiddqui@users.noreply.github.com>
2024-11-06 20:49:45 +0500

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '@/components/Header';
-import Hero1 from '@/components/Hero1';
+import Header from '../components/Header';
+import Hero1 from '../components/Hero1';
 import { Libre_Bodoni } from '@next/font/google';
 
 const libreBodoni = Libre_Bodoni({
